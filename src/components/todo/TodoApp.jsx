5991720a9750c7510751360efd34f187c8946f45
JEdwards <jordanscottedwards@gmail.com>
2023-02-21 16:41:07 -0500

    Added event handling for login component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
+import './TodoApp.css'
+
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            Todo Management Application
             <LoginComponent/>
             {/* <WelcomeComponent/> */}
         </div>
@@ -9,16 +11,28 @@ export default function TodoApp() {
 }
 
 function LoginComponent() {
+    
+    const[username, setUsername] = useState('user')
+    const[password, setPassword] = useState('default')
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
-                    <label>User Name</label>
-                    <input type="text" name="username"/>
+                    <label>User Name:</label>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
                 </div>
                 <div>
-                    <label>Password</label>
-                    <input type="password" name="password"/>
+                    <label>Password:</label>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
                     <button type="button" name="login">login</button>
