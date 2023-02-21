5cdf66ecad3ef70762749291bc83891ee2d90993
JEdwards <jordanscottedwards@gmail.com>
2023-02-20 22:00:50 -0500

    Added state to counter

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react'
 import './Counter.css'
 
 export default function Counter() {
 
+    const [count, setCount] = useState(0);
+    
     function incrementCounterFunction() {
-        console.log('increment clicked')
+        setCount(count + 1)
+        console.log(count)
     }
 
     return (
         <div className="Counter">
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>
                 <button className="counterButton" 
                         onClick={incrementCounterFunction}
