a5da62ef51eae4f9379d064d06aef344d5aeccdf
JEdwards <jordanscottedwards@gmail.com>
2023-02-22 16:01:57 -0500

    Refactored Components

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,10 @@
-import { useState } from 'react'
-import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import LogoutComponent from './LogoutComponent'
+import HeaderComponent from './HeaderComponent'
+import ListTodosComponent from './ListTodosComponent'
+import WelcomeComponent from './WelcomeComponent'
+import ErrorComponent from './ErrorComponent'
+import LoginComponent from './LoginComponent'
 import './TodoApp.css'
 
 export default function TodoApp() {
@@ -16,175 +21,21 @@ export default function TodoApp() {
                     <Route path='/todos' element={<ListTodosComponent />} />
                     <Route path='/logout' element={<LogoutComponent />} />
                 </Routes>
-                <FooterComponent />
             </BrowserRouter>
 
         </div>
     )
 }
 
-function LoginComponent() {
-    
-    const[username, setUsername] = useState('user')
-    const[password, setPassword] = useState('default')
-    const[showSuccessMessage, setShowSuccessMessage] = useState(false)
-    const[showErrorMessage, setShowErrorMessage] = useState(false)
-    const navigate = useNavigate()
 
-    function handleUsernameChange(event) {
-        setUsername(event.target.value);
-    }
 
-    function handlePasswordChange(event) {
-        setPassword(event.target.value);
-    }
 
-    function handleSubmit() {
-        if (username === 'user' && password === 'default') {
-            console.log('Success')
-            setShowSuccessMessage(true)
-            setShowErrorMessage(false)
-            navigate(`/welcome/${username}`)
-        } else {
-            console.log('Failed')
-            setShowErrorMessage(true)
-            setShowSuccessMessage(false)
-        }
-    }
 
-    return (
-        <div className="Login">
-            <h1>Please log in</h1>
-            {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
-            {showErrorMessage && <div className="errorMessage">Authentication Failed. Please check your credentials.</div>}
-            <div className="LoginForm">
-                <div>
-                    <label>User Name:</label>
-                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
-                </div>
-                <div>
-                    <button type="button" name="login" onClick={handleSubmit}>login</button>
-                </div>
-            </div>
-        </div>
-    )
-}
 
-function WelcomeComponent() {
-    
-    const {username} = useParams()
-    console.log(username)
-    
-    return (
-        <div className="Welcome">
-            <h1>Welcome {username}</h1>
-            <div>
-                Manage Your Todos - <Link to="/todos">Here</Link>
-            </div>
-        </div>
-    )
-}
 
-function ErrorComponent() {
-    return (
-        <div className="ErrorComponent">
-            <h1>We are working really hard!</h1>
-            <div>
-                Apologies for the 404.
-            </div>
-        </div>
-    )
-}
 
-function LogoutComponent() {
-    return (
-        <div className="LogoutComponent">
-            <h1>You are logged out</h1>
-            <div>
-                Thanks for using the app.
-            </div>
-        </div>
-    )
-}
 
-function ListTodosComponent() {
-    
-    const today = new Date()
-    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
-    
-    const todos = [
-        {id: 1, description: 'Learn AWS', done: false, targetDate: targetDate},
-        {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate},
-        {id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate}
-    ]
-    
-    return (
-        <div className="container">
-            <h1>Things You Want To do!</h1>
-            <div>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <td>ID</td>
-                            <td>Description</td>
-                            <td>Is Done?</td>
-                            <td>Target Date</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        todos.map(
-                            todo => (
-                                <tr key={todo.id}>
-                                    <td>{todo.id}</td>
-                                    <td>{todo.description}</td>
-                                    <td>{todo.done.toString()}</td>
-                                    <td>{todo.targetDate.toDateString()}</td>
-                                </tr>
-                            )
-                        )
-                    } 
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
-}
 
-function HeaderComponent() {
-    return (
-        <header className="border-bottom border-light border-5 mb-5 p-2">
-            <div className="container">
-                <div className="row">
-                    <nav className="navbar navbar-expand-lg">
-                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.github.com/jse955">EdEnterprises</a>
-                        <div className="collapse navbar-collapse">
-                            <ul className="navbar-nav">
-                                <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/user">Home</Link></li>
-                                <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
-                            </ul>
-                        </div>
-                        <ul className="navbar-nav">
-                            <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
-                            <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </header>
-    )
-}
 
-function FooterComponent() {
-    return (
-        <footer className="footer">
-            <div className="container">
-                Footer
-            </div>
-        </footer>
-    )
-}
+
+
