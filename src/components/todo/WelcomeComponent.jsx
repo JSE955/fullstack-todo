6abe61ba40984429fd037c5283ff421c5f8270d5
JEdwards <jordanscottedwards@gmail.com>
2023-02-23 11:00:33 -0500

    Implemented Axios with GET call

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,14 +1,36 @@
 import {useParams, Link} from 'react-router-dom'
+import axios from 'axios'
 
 function WelcomeComponent() {
     
     const {username} = useParams()
+
+    function callHelloWorldRestApi() {
+        console.log('called')
+        axios.get('http://localhost:8080/hello-world')
+            .then((response) => successfulResponse(response))
+            .catch((error) => errorResponse(error))
+            .finally( () => console.log('cleanup'))
+
+    }
+
+    function successfulResponse(response) {
+        console.log(response)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
     
     return (
         <div className="Welcome">
             <h1>Welcome {username}</h1>
             <div>
                 Manage Your Todos - <Link to="/todos">Here</Link>
+            </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+                    Call Hello World</button>
             </div>
         </div>
     )
